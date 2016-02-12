333909d7c4946295b30c152fc6771eb0b14f78dc
Andreas Ernhofer <aernhofer@student.tgm.ac.at>
2016-02-12 10:26:27 +0100

    Initial Commit

---
+++ src/com/prgguru/jersey/Constants.java
@@ -0,0 +1,9 @@
+package com.prgguru.jersey;
+//Change these parameters according to your DB
+public class Constants {
+	public static String dbClass = "com.mysql.jdbc.Driver";
+	private static String dbName= "users";
+	public static String dbUrl = "jdbc:mysql://debian:3306/"+dbName;
+	public static String dbUser = "user";
+	public static String dbPwd = "user";
+}
